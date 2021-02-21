a243bcdd5ea985be4d4e82caced29a159235aff8
alexmi1972 <emanuel_b1998@yahoo.com>
2021-02-21 22:35:40 +0200

    Final Project

--- main.py
+++ main.py
@@ -1,59 +1,104 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import xlsxwriter
+import pandas as pd
 
-def get_emag_product_type(nume_produs):
-    url = requests.get(f"https://www.emag.ro/{nume_produs}/c?tree_ref=2172&ref=cat_tree_91")
-    return bs(url.content)
+name_list = []
+old_price_list = []
+new_price_list = []
+discount_list = []
+reviews_list = []
+link_list = []
 
-def get_product_information():
-    product = {"PRODUCT NAME": None,
+product = {"PRODUCT NAME": None,
                "OLD PRICE": None,
                "NEW PRICE": None,
                "DISCOUNT": None,
                "REVIEWS": None,
                "LINK": None}
-    name_list = []
-    old_price_list = []
-    new_price_list = []
-    discount_list = []
-    reviews_list = []
-    link_list = []
 
-    for product_code in soup.find_all("div", class_="card-item js-product-data"):
+def get_emag_product_url(nume_produs,numar_pagini):
+    url = requests.get(f"https://www.emag.ro/{nume_produs}/p{numar_pagini}/c")
+    return bs(url.content)
+
+def get_product_information(html_code):
+
+    for product_code in html_code.find_all("div", class_="card-item js-product-data"):
         name = product_code["data-name"]
         name_list.append(name)
         try:
             old_price = product_code.find("p", class_="product-old-price").s.text
             old_price_list.append(old_price)
         except AttributeError:
-            old_price_list.append("No discount for this product")
+            old_price_list.append("No discount")
         new_price = product_code.find("p", class_="product-new-price").text
         new_price_list.append(new_price)
         try:
             discount = product_code.find("span", class_="product-this-deal").text
             discount_list.append(discount)
         except AttributeError:
-            discount_list.append("No discount for this product")
+            discount_list.append("No discount")
         link = product_code.find("a", class_="product-title js-product-url")["href"]
         link_list.append(link)
+        try:
+            reviews = product_code.find("div", class_="star-rating-text").span.text
+            reviews_list.append(reviews)
+        except AttributeError:
+            reviews_list.append("No reviews")
 
-        product["PRODUCT NAME"] = name_list
-        product["OLD PRICE"] = old_price_list
-        product["NEW PRICE"] = new_price_list
-        product["DISCOUNT"] = discount_list
-        product["REVIEWS"] = reviews_list
-        product["LINK"] = link_list
+    return True
 
+def sort_information(column):
+    dataframe = pd.DataFrame(product)
+    dataframe.sort_values(by=column, ascending=True)
+
+def save_information_in_excel():
     workbook = xlsxwriter.Workbook("produse.xlsx")
     worksheet = workbook.add_worksheet()
     col_num = 0
     for key, value in product.items():
-        worksheet.write(0,col_num,key)
-        worksheet.write_column(1,col_num,value)
+        worksheet.write(0, col_num, key)
+        worksheet.write_column(1, col_num, value)
         col_num += 1
-
     workbook.close()
+    return True
+
+
+def main():
+
+
+    print("!!!WEB SCRAPPING EMAG.RO!!!")
+    nume_produs = input("Introduceti numele produsului>> ")
+    numar_pagini = int(input("Introduceti numarul de pagini emag>> "))
+    count = 1
+    while count <= numar_pagini:
+        print(count)
+        url = get_emag_product_url(nume_produs, str(count))
+        get_product_information(url)
+        count += 1
+
+    product["PRODUCT NAME"] = name_list
+    product["OLD PRICE"] = old_price_list
+    product["NEW PRICE"] = new_price_list
+    product["DISCOUNT"] = discount_list
+    product["REVIEWS"] = reviews_list
+    product["LINK"] = link_list
+
+    dataframe = pd.DataFrame(product)
+    dataframe.sort_values(by="PRODUCT NAME", ascending=True, kind="mergesort")
+    print(dataframe)
+
+
+
+    while True:
+        choice = input("Doriti sa salvati informatiile din web scraping intr-un fisier excel ? (Y/N)>>")
+        if choice == "Y":
+            save_information_in_excel()
+            break
+        elif choice == "N":
+            break
+        else:
+            print("Nu am inteles!!!")
 
 
 
@@ -61,47 +106,7 @@ def get_product_information():
 
 
 
+main()
 
-
-
-
-soup = get_emag_product_type("telefoane-mobile")
-get_product_information()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# driver = webdriver.Chrome(executable_path="C:/Users/Gh0sT/Desktop/chromedriver_win32")
-# url = "https://www.emag.ro/"
-# driver.get(url)
-#
-# def get_url(search_term):
-#     template = "https://www.emag.ro/{}/c?tree_ref=2172&ref=cat_tree_91"
-#     search_term = search_term.replace(" ", "+")
-#     return template.format(search_term)
-#
-# url = get_url("laptopuri")
-# print(url)
-
-
-
-# workbook = Workbook()
-# sheet = workbook.active
-#
-# sheet["A1"] = "hasdasd"
-# sheet["B1"] = "world!"
-#
-# workbook.save(filename="hello_world.xlsx")
-
+df = pd.DataFrame(product)
+df.sort_values
